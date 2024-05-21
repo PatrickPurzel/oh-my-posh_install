576b392eaaeef59a134a69d85d572b16501e24e7
PatrickPurzel <patrick@budelmann-mail.de>
2024-05-21 09:42:30 +0200

    change 1st line

--- linux/install.sh
+++ linux/install.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-curl -s https://ohmyposh.dev/install.sh | bash -s
+#!/usr/bin/env bash
+curl -s https://ohmyposh.dev/install.sh && bash -s
 oh-my-posh font install
 oh-my-posh get shell
 echo "eval "$(oh-my-posh init bash)"" >> ~/.bashrc || echo "eval "$(oh-my-posh init bash)"" >> ~/.profile || echo "eval "$(oh-my-posh init bash)"" >> ~/.bash_profile
