fb0905b7b606de8542a67dd5b4eda7d594fb138c
PatrickPurzel <patrick@budelmann-mail.de>
2024-05-21 11:07:13 +0200

    linux init

--- linux/install.sh
+++ linux/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -s https://ohmyposh.dev/install.sh && bash -s
-oh-my-posh font install
+oh-my-posh font install FiraMono
 oh-my-posh get shell
-echo "eval "$(oh-my-posh init bash)"" >> ~/.bashrc || echo "eval "$(oh-my-posh init bash)"" >> ~/.profile || echo "eval "$(oh-my-posh init bash)"" >> ~/.bash_profile
+echo 'eval "$(oh-my-posh init bash)"' >> ~/.bashrc || echo 'eval "$(oh-my-posh init bash)"' >> ~/.profile || echo 'eval "$(oh-my-posh init bash)"' >> ~/.bash_profile
 exec bash
 oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/aliens.omp.json'
