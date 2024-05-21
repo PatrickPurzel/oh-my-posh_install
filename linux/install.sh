1c9a203fb3cb37656dc7c435638796f3b74e2a4d
PatrickPurzel <patrick@budelmann-mail.de>
2024-05-21 09:40:13 +0200

    change init bash

--- linux/install.sh
+++ linux/install.sh
@@ -4,4 +4,4 @@ oh-my-posh font install
 oh-my-posh get shell
 echo "eval "$(oh-my-posh init bash)"" >> ~/.bashrc || echo "eval "$(oh-my-posh init bash)"" >> ~/.profile || echo "eval "$(oh-my-posh init bash)"" >> ~/.bash_profile
 exec bash
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/aliens.omp.json' | Invoke-Expression
+oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/aliens.omp.json'
